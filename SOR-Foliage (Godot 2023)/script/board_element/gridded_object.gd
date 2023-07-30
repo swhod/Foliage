7e00cd6d57ef316706b60fa5d847d70ce2148587
Anchor R <87940242+swhod@users.noreply.github.com>
2023-07-30 11:06:35 +0800

    Rewrite GriddedObject (unfinished)

--- SOR-Foliage (Godot 2023)/script/board_element/gridded_object.gd
+++ SOR-Foliage (Godot 2023)/script/board_element/gridded_object.gd
@@ -1,46 +1,53 @@
 class_name GriddedObject
-extends KeyValuePack
-## Designed for objects gridded into bodies.
-## Each body occupies exactly one cell of grid. 
-## To describe bodies, use [KeyValuePack] with every key to be 
-## a bodytype, and corresponding value to be a position vector.
+extends RefCounted
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # (private)
-# The vector that indicates the position.
-var _position := Vector2.ZERO
+# The zipped data describing all bodies.
+var _bodyzip = {}
 
 # (private)
 # The bool value of permission to move this object.
 var _movable := true
 
 # (private)
-# The array of permitted bodytypes.
-var _permitted := []
+# The array of all bodies permitted.
+var _permitted_bodies := []
 
 # (private)
-# The input mapper for bodypack data.
-func _input_mapper(pair):
-	if pair[KEY] in self._permitted and pair[VALUE] is Vector2:
-		return [pair[KEY], pair[VALUE] - self._position]
-	else:
-		return []
-
-# (private)
-# The output mapper for bodypack data.
-func _output_mapper(pair):
-	return [pair[KEY], pair[VALUE] + self._position]
+# The vector that indicates the position.
+var _position := Vector2.ZERO
 
 # (initialization)
 # The constructor of this class when instantiating.
-func _init(bodypack = {}, init_movable := true, init_permitted := []):
-	self._permitted = init_permitted if init_permitted else \
-			KeyValuePack.new(bodypack).to_dict().keys()
-	self._permitted.make_read_only()
-	self._movable = init_movable
-	self._position = Vector2.ZERO
-	super(bodypack, Callable(self, "_input_mapper"), Callable(self, "_output_mapper"))
+func _init(init_bodypack := {}):
+	self._position = self._init_position()
+	self._permitted_bodies = self._init_permitted_bodies()
+	self._permitted_bodies.make_read_only()
+	self._movable = self._init_movable()
+	self._bodyzip = self._init_bodyzip()
+	self.add_bodypack(init_bodypack)
+
+# (initialization, virtual)
+## Returns the initial position.
+func _init_position() -> Vector2:
+	return Vector2.ZERO
+
+# (initialization, virtual)
+## Returns an array of all bodies permitted.
+func _init_permitted_bodies() -> Array:
+	return []
+
+# (initialization, virtual)
+## Returns the initial state of movability.
+func _init_movable() -> bool:
+	return true
+
+# (initialization, virtual)
+## Returns the initial bodyzip.
+func _init_bodyzip():
+	return {}
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -49,13 +56,69 @@ func _init(bodypack = {}, init_movable := true, init_permitted := []):
 func is_movable() -> bool:
 	return self._movable
 
-# (writing)
-## Set movability to given state.
-func set_movable(movable := true):
-	self._movable = movable
+# (read-only)
+## Returns the body pack of this object.
+func get_bodypack() -> Dictionary:
+	return self._to_bodypack(self._bodyzip)
 
 # (writing)
-## Move this object by given displacement.
+## Move this object by [param displacement].
 func do_move(displacement := Vector2.ZERO):
 	if self._movable:
 		self._position += displacement
+
+# (writing)
+## Add bodies given by [param bodypack].
+func add_bodypack(bodypack: Dictionary):
+	return self._add_bodyzip(self._to_bodyzip(bodypack))
+
+# (writing)
+## Delete bodies given by [param bodypack].
+func delete_bodypack(bodypack: Dictionary):
+	return self._delete_bodyzip(self._to_bodyzip(bodypack))
+
+# (private)
+# Returns the body pack according to given bodyzip.
+func _to_bodypack(bodyzip) -> Dictionary:
+	var unzipped := self._unzip(bodyzip)
+	var bodypack := {}
+	for b in unzipped:
+		bodypack[b] = unzipped[b].map(func(pos): return pos + self._position)
+	return bodypack
+
+# (private)
+# Returns the body zip according to given bodypack.
+func _to_bodyzip(bodypack: Dictionary):
+	var not_zipped_yet := {}
+	for b in bodypack:
+		if b not in self._permitted_bodies: continue
+		not_zipped_yet[b] = bodypack[b].map(func(pos): return pos - self._position)
+	return self._zip(not_zipped_yet)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# (virtual)
+## Returns the body pack unzipped from [param bodyzip].
+func _unzip(bodyzip) -> Dictionary:
+	return bodyzip
+
+# (virtual)
+## Returns the body zip unzipped from [param bodypack].
+func _zip(bodypack: Dictionary):
+	return bodypack
+
+# (writing, virtual)
+## Add bodies specified by [param bodyzip].
+func _add_bodyzip(bodyzip):
+	for b in bodyzip:
+		for pos in bodyzip[b]:
+			if not self._bodyzip[b].has(pos):
+				self._bodyzip[b].append(pos)
+
+# (writing, virtual)
+## Delete bodies specified by [param bodyzip].
+func _delete_bodyzip(bodyzip):
+	for b in bodyzip:
+		if not self._bodyzip.has(b): continue
+		for pos in bodyzip[b]:
+			self._bodyzip[b].erase(pos)
