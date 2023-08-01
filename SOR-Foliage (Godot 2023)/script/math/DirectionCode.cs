b908b59cfdd19a04d470b7e05fec091df064e7c7
Anchor R <87940242+swhod@users.noreply.github.com>
2023-08-01 11:07:21 +0800

    Update DirectionCode.cs

--- SOR-Foliage (Godot 2023)/script/math/DirectionCode.cs
+++ SOR-Foliage (Godot 2023)/script/math/DirectionCode.cs
@@ -4,11 +4,23 @@
 public class DirectionCode
 {
     private byte _code = 0;
+    public byte Code { get => _code; set => _code = (byte)(value & 0b_1111); }
     public bool Right { get => (_code & 1) == 1; }
     public bool Up { get => ((_code >> 1) & 1) == 1; }
     public bool Left { get => ((_code >> 2) & 1) == 1; }
-    public bool Down { get => ((_code >> 3) & 1) == 1; }
+    public bool Down { get => (_code >> 3) == 1; }
+
+    public DirectionCode(byte code) => Code = code;
 
     public DirectionCode(bool right, bool up, bool left, bool down) 
-        => _code = (byte)((right? 1: 0) + (up? 2: 0) + (left? 4: 0) + (down? 8: 0));
+        => Code = (byte)((right? 1: 0) + (up? 2: 0) + (left? 4: 0) + (down? 8: 0));
+    
+    public static DirectionCode operator &(DirectionCode d1, DirectionCode d2)
+        => new DirectionCode((byte)(d1._code & d2._code));
+    
+    public static DirectionCode operator |(DirectionCode d1, DirectionCode d2)
+        => new DirectionCode((byte)(d1._code | d2._code));
+    
+    public static DirectionCode operator ^(DirectionCode d1, DirectionCode d2)
+        => new DirectionCode((byte)(d1._code ^ d2._code));
 }
