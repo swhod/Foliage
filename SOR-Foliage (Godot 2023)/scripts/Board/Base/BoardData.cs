3bcf1250c48ae67b42d3c08c22f1d88964dd69a9
Anchor R <87940242+swhod@users.noreply.github.com>
2023-09-15 23:34:38 +0800

    Create BoardData.cs

---
+++ SOR-Foliage (Godot 2023)/scripts/Board/Base/BoardData.cs
@@ -0,0 +1,90 @@
+using Vector2 = System.Numerics.Vector2;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Foliage.Board.Base;
+
+public class BoardData
+{
+    /// <summary>
+    /// Collects all the structures on this board.
+    /// </summary>
+    public IList<IStructural> Structures => _structures;
+    /// <summary>
+    /// Inspects a tile to know about all structures on it.
+    /// </summary>
+    /// <param name="position">The position of the tile inspected.</param>
+    /// <returns>All structures on the tile inspected.</returns>
+    public List<IStructural> Inspect(Vector2 position)
+        => _tiles.ContainsKey(position)? _tiles[position].ToList(): new();
+    
+    public BoardData()
+    {
+        (_structures, _tiles, _updates) = (new(), new(), new());
+        _structures.CollectionChanged += StructuresChanged;
+    }
+
+    private ObservableCollection<IStructural> _structures { get; }
+    private Dictionary<Vector2, List<IStructural>> _tiles { get; }
+    private Dictionary<IStructural, NotifyCollectionChangedEventHandler> _updates { get; }
+    
+    private void StructuresChanged(object sender, 
+                                   NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Move) return;
+        if (e.Action == NotifyCollectionChangedAction.Reset)
+        {
+            _tiles.Clear();
+            return;
+        }
+        foreach(IStructural structure in e.OldItems)
+        {
+            foreach(KeyValuePair<Vector2, IFunctional> tile 
+                    in structure.OwningBody.Tiles)
+            {
+                Vector2 position = tile.Key;
+                structure.OwningBody.PositionsChanged -= _updates[structure];
+                _updates.Remove(structure);
+                _tiles[position].Remove(structure);
+                if (_tiles[position].Count == 0) _tiles.Remove(position);
+            }
+        }
+        foreach(IStructural structure in e.NewItems)
+        {
+            foreach(KeyValuePair<Vector2, IFunctional> tile
+                    in structure.OwningBody.Tiles)
+            {
+                Vector2 position = tile.Key;
+                if (_tiles.ContainsKey(position)) _tiles[position] = new();
+                _tiles[position].Add(structure);
+                _updates[structure] = (sender, e) => Update(structure, sender, e);
+                structure.OwningBody.PositionsChanged += _updates[structure];
+            }
+        }
+    }
+
+    private void Update(IStructural structure, 
+                        object sender, 
+                        NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Move) return;
+        if (e.Action == NotifyCollectionChangedAction.Reset)
+        {
+            foreach(KeyValuePair<Vector2, List<IStructural>> tile 
+                    in _tiles) tile.Value.Remove(structure);
+            return;
+        }
+        foreach(Vector2 position in e.OldItems)
+        {
+            _tiles[position].Remove(structure);
+            if (_tiles[position].Count == 0) _tiles.Remove(position);
+        }
+        foreach(Vector2 position in e.NewItems)
+        {
+            if (_tiles.ContainsKey(position)) _tiles[position] = new();
+            _tiles[position].Add(structure);
+        }
+    }
+}
