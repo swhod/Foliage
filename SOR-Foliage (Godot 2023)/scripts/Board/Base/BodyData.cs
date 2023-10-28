b640dc6e0690fe15331b06f3b784be49fc0100dd
Anchor R <87940242+swhod@users.noreply.github.com>
2023-10-28 16:22:11 +0800

    Update BodyData.cs

--- SOR-Foliage (Godot 2023)/scripts/Board/Base/BodyData.cs
+++ SOR-Foliage (Godot 2023)/scripts/Board/Base/BodyData.cs
@@ -12,7 +12,7 @@ public class BodyData
     /// <summary>
     /// Collects all the positions.
     /// </summary>
-    public IReadOnlyCollection<Vector2> Positions => _positions;
+    public IList<Vector2> Positions => _positions;
     /// <summary>
     /// Represents the offset from aligned with tile grid.
     /// </summary>
@@ -21,24 +21,23 @@ public class BodyData
     /// Represents the board linked with this BodyData.
     /// </summary>
     public BoardData Board => _board;
+    /// <summary>
+    /// Moves this BodyData by given vector.
+    /// </summary>
+    /// <param name="vector">The vector to move by.</param>
+    public void Move(Vector2 vector)
+    {
+        _offset += vector;
+        while (_offset.X <  OffsetMin) ShiftOffsetBy(Directions.Right);
+        while (_offset.Y <  OffsetMin) ShiftOffsetBy(Directions.Down);
+        while (_offset.X >= OffsetMax) ShiftOffsetBy(Directions.Left);
+        while (_offset.Y >= OffsetMax) ShiftOffsetBy(Directions.Up);
+    }
 
     public event NotifyCollectionChangedEventHandler PositionsChanged
     {
         add => _positions.CollectionChanged += value;
         remove => _positions.CollectionChanged -= value;
-    }
-
-    public void Add(Vector2 position)
-        => _positions.Add(Floor(position));
-    public void Remove(Vector2 position)
-        => _positions.Remove(Floor(position));
-    public void Move(Vector2 distance)
-    {
-        _offset += distance;
-        while (_offset.X <  OffsetMin) ShiftOffset(Directions.Right);
-        while (_offset.Y <  OffsetMin) ShiftOffset(Directions.Down);
-        while (_offset.X >= OffsetMax) ShiftOffset(Directions.Left);
-        while (_offset.Y >= OffsetMax) ShiftOffset(Directions.Up);
     }
     public void LinkWith(BoardData board)
     {
@@ -52,19 +51,17 @@ public class BodyData
     private ObservableCollection<Vector2> _positions { get; }
     private Vector2 _offset = Vector2.Zero;
     private BoardData _board = null;
-    private void ShiftOffset(Directions shiftDirection)
+    private void ShiftOffsetBy(Directions direction)
     {
-        Vector2 shiftVector = shiftDirection.DirectionsToVector2();
-        _offset += shiftVector;
+        Vector2 vector = direction.DirectionsToVector2();
+        _offset += vector;
         foreach (Vector2 position in _positions)
         {
-            Remove(position);
-            Add(position - shiftVector);
+            _positions.Remove(position);
+            _positions.Add(position - vector);
         }
     }
 
-    private static Vector2 Floor(Vector2 vector)
-        => new((float)System.Math.Floor(vector.X), (float)System.Math.Floor(vector.Y));
     private const float OffsetMargin = 0.1f;
     private const float OffsetMin = 0.0f - OffsetMargin;
     private const float OffsetMax = 1.0f + OffsetMargin;
